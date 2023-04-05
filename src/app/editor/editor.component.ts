14342636d089c1a3ea728033c56b056b43ab5c93
lakshya <102359158+lakshyadown@users.noreply.github.com>
2023-04-05 21:44:26 +0530

    add editor module

---
+++ src/app/editor/editor.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fl-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss'],
+})
+export class EditorComponent {}
