73f01983440070fc23e98ae9337eec1c37f78e4e
lakshya <lakshyadahiya0246@gmail.com>
2023-04-09 18:10:34 +0530

    add input restriction directive

---
+++ src/app/editor/restrict-input.directive.ts
@@ -0,0 +1,39 @@
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[flRestrictInputTo]',
+})
+export class RestrictInputDirective {
+  readonly NUMBER_ONLY_REGEX = /[^0-9]*/g;
+  readonly STRING_ONLY_REGEX = /[^0-9]*/g;
+  @Input('flRestrictInputTo') restrictionType!: 'number' | 'string';
+  @Output('safeChange') change = new EventEmitter<string>();
+  constructor() {}
+
+  @HostListener('input', ['$event'])
+  onInputChange(event: Event) {
+    if (!Boolean(event.target)) {
+      return;
+    }
+    const eventTarget = event.target as HTMLInputElement;
+    const initialValue = eventTarget.value;
+    eventTarget.value = initialValue.replace(
+      this.restrictionType === 'number'
+        ? this.NUMBER_ONLY_REGEX
+        : this.STRING_ONLY_REGEX,
+      ''
+    );
+    if (initialValue !== eventTarget.value) {
+      event.stopPropagation();
+    } else {
+      console.log({ target: (event.target as HTMLInputElement).value });
+      this.change.emit((event.target as any).value);
+    }
+  }
+}
